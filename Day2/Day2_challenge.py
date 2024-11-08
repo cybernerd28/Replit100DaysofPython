4879d519440692dfecfcf0acc1c1e3a30f3a8d16
Warmann <onisowarmann@proton.me>
2024-11-08 05:20:41 -0500

    variables

---
+++ Day2/Day2_challenge.py
@@ -0,0 +1,18 @@
+# Write code that does the following
+
+# Asks for the user's name, favorite food, favorite music and where they live (or you can create other questions!)
+# Store all the answers in different variables.
+# Print out a full sentence that includes the user's favorite things.
+# Give them a positive affirmation at the end - tell them they rock at doing something
+
+
+name = input("name: ")
+food = input("favorite food: ")
+music = input("favorite music: ")
+location = input("where do you live: ")
+
+print()
+print("Getting to know you!")
+print("You are\n" + name + "\nYou\'re probably hungry for\n" + food +
+      "\nand you\'re definitely getting your groove on to\n" + music +
+      " living in the amazing\n" + location)
---
+++ Day2/variables.py
@@ -0,0 +1,22 @@
+myName = input("What's you name? ")
+myAge = input("How old are you? ")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit? ")
+print("OF COURSE YOU DO!")
+
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Replit is")
+print(replit)
+
+# Common Errors
+## Syntax Error
+# my variable = input("WHO GOES THERE? ")
+# print(my variable)
+
+## Name Error
+# myGrandma = input("How's your Grandma doing? ")
+# print(mygrandma)
